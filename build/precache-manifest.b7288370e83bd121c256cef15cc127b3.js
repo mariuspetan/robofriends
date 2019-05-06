c81b53a85f30b9e2dce4449b81669dd84c928533
mpetan <mpetan@connexservice.ca>
2019-05-05 23:23:04 -0400

    working version first import

---
+++ build/precache-manifest.b7288370e83bd121c256cef15cc127b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6f50d1ee160d9e6d8e63148b2d80f2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "bd01e9e1202213437512",
+    "url": "/static/css/main.602d5b22.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "bd01e9e1202213437512",
+    "url": "/static/js/main.cb8aaab4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
